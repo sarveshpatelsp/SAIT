76ba10bd3219a2f29aa96706a05a7ab9113a3d6e
Sarvesh Patel <sarveshpatelsp91@gmail.com>
2022-09-13 22:47:36 +0530

    Today, I made an OTP screen and used Pinview Library for OTP recieving design. And implemented explicit intent on forgot password clicking.

--- app/src/main/java/com/akkupatel/loginpage/MainActivity.java
+++ app/src/main/java/com/akkupatel/loginpage/MainActivity.java
@@ -9,15 +9,25 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView textView;
+    TextView textView1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.createNewAccount);
+        textView1 = findViewById(R.id.forgotPassword);
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, SignUp.class);
+                startActivity(intent);
+            }
+        });
+
+        textView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, OtpScreen.class);
                 startActivity(intent);
             }
         });
