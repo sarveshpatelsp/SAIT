b44bc053aa20da117ea243fddfcfecc097b2f0b1
Sarvesh Patel <sarveshpatelsp91@gmail.com>
2022-09-17 20:22:25 +0530

    Today just added finish() method inside the AskfromUser screen. So that whenever I press back button in home Screen. I directly come out of the app. And also connected Firebase Database with Android Studio manually.

---
+++ app/src/main/java/com/akkupatel/loginpage/Profile.java
@@ -0,0 +1,16 @@
+package com.akkupatel.loginpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+    }
+}
